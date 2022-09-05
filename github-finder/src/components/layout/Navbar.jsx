e05a7305afe27b96f9a3186fd7c20a0587c4751a
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-09-05 00:06:27 +0000

    Footer component

--- github-finder/src/components/layout/Navbar.jsx
+++ github-finder/src/components/layout/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 function Navbar( {title} ) {
-  return (
+  return ( 
     <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content' >
         <div className='container mx-auto'>
             <div className='flex-none px-2 mx-2'>
