8d1093b9f1f3cdc18f42c6285d7b83832770d151
Alhassan Joshua Abel <saintjosh26@gmail.com>
2022-09-19 10:28:52 +0000

    display user feature

--- github-finder/src/pages/Home.jsx
+++ github-finder/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import UserResults from '../components/users/UserResults'
 function Home() {
   return (
     <div>
-        <h1 className="text-6xl">Welcome</h1>
+        {/* <h1 className="text-6xl">Welcome</h1> */}
         {/* {process.env.REACT_APP_GITHUB_TOKEN} */}
         <UserResults />
     </div>
